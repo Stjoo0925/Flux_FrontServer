36160bc5079b8fd1f52ac304bdd4e0c1f9d2484b
Stjoo0925 <stjoo0925@gmail.com>
2024-07-23 10:57:52 +0900

    build : bootstrap update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
-<script setup></script>
-
 <template>
-  <h1>FLUX</h1>
+  <div id="app">
+    <Header />
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+import Header from './components/header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+/* 추가 스타일이 필요하다면 여기에 작성 */
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
 import './assets/font.css'
 import './assets/reset.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
