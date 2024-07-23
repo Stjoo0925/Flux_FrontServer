35ade62bde3dfe088c7fef87a102ce9d92d1bc2e
Jin-tonix <jinheemok815@gmail.com>
2024-07-23 20:15:35 +0900

    feat:paymentpages

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <Header />
+    <Header />,
   </div>
 </template>
 
 <script>
-import Header from './components/header.vue'
-
+import Header from './components/Header.vue'
+  
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
   }
 }
 </script>
