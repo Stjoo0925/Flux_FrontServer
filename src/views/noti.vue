5ead0abdfdb3d665510c6ac3f6aebdb46aaa2edb
Stjoo0925 <stjoo0925@gmail.com>
2024-07-27 18:44:21 +0900

    ✏️ fix : footer sticky update

---
+++ src/views/noti.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Header from "@/components/header/flux_component_header.vue";
+import Footer from "@/components/footer/flux_component_footer.vue";
+</script>
+<template>
+  <Header />
+    <div class="section">
+      <Section />
+    </div>
+  <Footer />
+</template>
+<style scoped>
+.section {
+  width: 100%;
+}
+.section-align {
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+}
+</style>
