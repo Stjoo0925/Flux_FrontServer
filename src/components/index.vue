fb685f4566779e1ad95e23f4e1b4f539a7f0eec1
Stjoo0925 <stjoo0925@gmail.com>
2024-07-27 22:43:22 +0900

    feat : banner img update

--- src/components/index.vue
+++ src/components/index.vue
@@ -4,13 +4,13 @@
     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="10000">
-          <img src="https://cdn.newscj.com/news/photo/202012/675804_677333_0328.jpg" class="d-block w-100" alt="...">
+          <img src="../assets/image/flux_main_banner1.webp" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item" data-bs-interval="2000">
-          <img src="https://cdn.newscj.com/news/photo/202012/675804_677333_0328.jpg" class="d-block w-100" alt="...">
+          <img src="../assets/image/flux_main_banner2.webp" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-          <img src="https://cdn.newscj.com/news/photo/202012/675804_677333_0328.jpg" class="d-block w-100" alt="...">
+          <img src="../assets/image/flux_main_banner3.webp" class="d-block w-100" alt="...">
         </div>
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
