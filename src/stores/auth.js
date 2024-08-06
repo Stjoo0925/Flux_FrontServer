c094df3922839102518499b70fbf5f772108feef
ppudding3861 <ppuding3861@gmail.com>
2024-08-06 10:40:10 +0900

    feat : login

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,20 +1,38 @@
 import { defineStore } from 'pinia';
 
 export const useAuthStore = defineStore('auth', {
-  state: () => ({
-    token: localStorage.getItem('authToken') || '',
-  }),
+  state: () => {
+    const authUser = localStorage.getItem('authUser');
+    let user = null;
+    try {
+      user = authUser ? JSON.parse(authUser) : null;
+    } catch (e) {
+      user = null;
+    }
+
+    return {
+      token: localStorage.getItem('authToken') || '',
+      user: user
+    };
+  },
   actions: {
     setToken(token) {
       this.token = token;
       localStorage.setItem('authToken', token);
     },
-    clearToken() {
+    setUser(user) {
+      this.user = user;
+      localStorage.setItem('authUser', JSON.stringify(user)); // 유저 정보 저장
+    },
+    clearAuth() {
       this.token = '';
+      this.user = null;
       localStorage.removeItem('authToken');
+      localStorage.removeItem('authUser');
     },
   },
   getters: {
     isAuthenticated: (state) => !!state.token,
   },
+  persist: true,
 });
