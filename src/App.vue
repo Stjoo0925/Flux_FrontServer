d036692a6bb38468557e80278e532e31854c7f37
ppudding3861 <ppuding3861@gmail.com>
2024-08-12 10:56:40 +0900

    chore : visit modify

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <script setup>
 import { useVisitorStore } from '@/stores/visitor';
+import { resetVisitCountAtMidnight } from '@/stores/resetVisitCount';
 
 const visitorStore = useVisitorStore();
 
 // 페이지 로드 시 한 번만 방문자 수를 증가시키도록 수정
 visitorStore.trackVisitor();
 
-// 00시에 방문자 기록 초기화 함수 추가
-function resetVisitCountAtMidnight() {
-  const now = new Date();
-  const millisecondsUntilMidnight =
-    new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0) - now;
-
-  setTimeout(() => {
-    localStorage.removeItem('lastVisitDate');
-    resetVisitCountAtMidnight(); // 다음 날 00시에 다시 실행
-  }, millisecondsUntilMidnight);
-}
-
+// 00시에 방문자 기록 초기화 함수 호출
 resetVisitCountAtMidnight();
 </script>
 
---
+++ src/stores/resetVisitCount.js
@@ -0,0 +1,13 @@
+// src/utils/resetVisitCount.js
+
+export function resetVisitCountAtMidnight() {
+    const now = new Date();
+    const millisecondsUntilMidnight =
+      new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0) - now;
+  
+    setTimeout(() => {
+      localStorage.removeItem('lastVisitDate');
+      resetVisitCountAtMidnight(); // 다음 날 00시에 다시 실행
+    }, millisecondsUntilMidnight);
+  }
+  
